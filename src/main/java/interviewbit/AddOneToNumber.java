a92daf085a1d8c4565ee83589323f2c730c86b6c
Aditya Karkara <aditya.karkara@gmail.com>
2022-07-17 23:14:52 +0530

    https://www.interviewbit.com/problems/add-one-to-number/

--- src/main/java/interviewbit/AddOneToNumber.java
+++ src/main/java/interviewbit/AddOneToNumber.java
@@ -2,7 +2,6 @@ package interviewbit;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class AddOneToNumber {
     public ArrayList<Integer> plusOne(ArrayList<Integer> A) {
